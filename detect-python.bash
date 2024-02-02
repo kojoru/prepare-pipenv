7aff49c0d623755624d0ad90ce8f5439719418ea
Konstantin Yakushev <Konstantin.Yakushev@wolterskluwer.com>
2024-02-02 11:10:23 +0100

    Update action.yml to include Python version detection

---
+++ detect-python.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REQUESTED_PYTHON_VERSION="$1"
+PYTHON_FULL_VERSION_PIPFILE=$(yq eval '.requires.python_full_version // ""' Pipfile --input-format toml --output-format csv 2>/dev/null || echo "")
+PYTHON_VERSION_PIPFILE=$(yq eval '.requires.python_version // ""' Pipfile --input-format toml --output-format csv 2>/dev/null || echo "")
+PYTHON_VERSION_INSTALLED=$(python -c "print(__import__('platform').python_version())")
+echo "Python versions:"
+echo "  - Requested version: $REQUESTED_PYTHON_VERSION"
+echo "  - Full version from Pipfile: $PYTHON_FULL_VERSION_PIPFILE"
+echo "  - Version from Pipfile: $PYTHON_VERSION_PIPFILE"
+echo "  - Installed version: $PYTHON_VERSION_INSTALLED"
+if [ -n "$REQUESTED_PYTHON_VERSION" ]; then
+    PYTHON_VERSION_TO_USE=$REQUESTED_PYTHON_VERSION
+elif [ -n "$PYTHON_FULL_VERSION_PIPFILE" ]; then
+    PYTHON_VERSION_TO_USE=$PYTHON_FULL_VERSION_PIPFILE
+elif [ -n "$PYTHON_VERSION_PIPFILE" ]; then
+    PYTHON_VERSION_TO_USE=$PYTHON_VERSION_PIPFILE
+else
+    PYTHON_VERSION_TO_USE=$PYTHON_VERSION_INSTALLED
+fi
+echo "Using Python version: $PYTHON_VERSION_TO_USE"
+echo "python-version=$PYTHON_VERSION_TO_USE" >> $GITHUB_OUTPUT
